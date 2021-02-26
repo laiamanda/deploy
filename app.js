214b4a4095e591d3ed2b7818a1a310b4ea7c1c47
laiamanda <40805270+laiamanda@users.noreply.github.com>
2021-02-26 13:39:44 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+app.get("/about", function(req,res){
+	res.render("about");
+});
+
+
+var port = process.env.PORT || 3000;
+
+app.listen(port,function(){
+	console.log("It's running");
+});
